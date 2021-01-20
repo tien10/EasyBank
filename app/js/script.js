f028769c67123c7c6c83b4bab527025f557cd525
Tien Nguyen <nguyentien7566@gmail.com>
2021-01-20 10:05:39 +0700

    part 2

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
-console.log('tiennvn');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function () {
+  console.log('click hamburger');
+
+  if (header.classList.contains('open')) {
+    // Close Hamburger Menu
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  } else {
+    // Open Hamburger Menu
+    header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
+  }
+});
